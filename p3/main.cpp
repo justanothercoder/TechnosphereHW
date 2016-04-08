3dd5524c43335425703896c68381a09ddb08aa6c
justanothercoder <yanushviktor@gmail.com>
2016-04-08 18:17:30 +0300

    Reading file

--- p3/main.cpp
+++ p3/main.cpp
@@ -1,12 +1,25 @@
+#include <fstream>
 #include "proxy.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-    const int PORT = 10000;
+    std::ifstream in(argv[1]);
+    std::vector<std::pair<IP, Port>> servers;
+    
+//    const int PORT = 10000;
+    int PORT;
+    in >> PORT;
+    
+    std::string ip;
+    int port;
+    while (in >> ip >> port) {
+        servers.emplace_back(boost::asio::ip::address::from_string(ip), port);
+    }
 
     boost::asio::io_service io_service;
 
-    Proxy proxy(io_service, PORT, {{boost::asio::ip::address::from_string("127.0.0.1"), 3100}});
+//    Proxy proxy(io_service, PORT, {{boost::asio::ip::address::from_string("127.0.0.1"), 3100}});
+    Proxy proxy(io_service, PORT, servers);
     proxy.run();
 
     io_service.run();
